0f3cd1cdd773568c84e0e59b81b7e41176dff3ce
Yigit Findik <yigit8@live.nl>
2023-01-13 12:23:41 +0100

    bezig nog niet klaar

---
+++ Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeoProfs.Models
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public int team_id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int position { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
+        [Display(Name = "Hire Date")]
+        [DataType(DataType.Date)]
+        public DateTime HireDate { get; set; }
+    }
+}
