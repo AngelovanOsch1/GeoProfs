f9726fa99dc3680e7f83381a68a9f5afb7155916
WessamNajjar <wnajjar00@bk.ru>
2023-01-20 10:25:15 +0100

    model aangemaakt voor de User

---
+++ Models/User.cs
@@ -0,0 +1,86 @@
+﻿//#define BeforeInheritance
+#if BeforeInheritance
+#region snippet_BeforeInheritance
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        public ICollection<Course> Courses { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
+#endregion
+#else
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class User : Person
+    {
+        [Key]
+        private int Id { get; set; }
+        
+        [ForeignKey("Id")]
+        public int TeamID { get; set; }
+
+        [ForeignKey("Id")]
+        public int Positie { get; set; }
+
+        public string Opmerkingen { get; set; }
+
+        public int VakantieDagen { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		[Display(Name = "End Date")]
+		public DateTime EndDate { get; set; }
+
+		[Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        public ICollection<Course> Courses { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
+#endif
